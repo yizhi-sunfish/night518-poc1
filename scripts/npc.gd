7a9c6f1b70c997d3b89eb35c4ea44d5cd4966ba3
yizhi-sunfish <201390179+yizhi-sunfish@users.noreply.github.com>
2025-05-03 15:28:44 +0200

    Done: read player and NPC states from json files

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,4 +1,4 @@
-# TODO: Almost the same as npc.gd, should refactor to avoid duplicated code
+# TODO: Almost the same as player.gd, should refactor to avoid duplicated code
 
 extends Node
 
@@ -9,18 +9,19 @@ var body_parts : Dictionary = {}
 var clothes : Dictionary = {}
 
 func _ready() -> void:
-	var player_info = FileProcessor.read_from_json_file("res://data/zh/player.json")
+	var player_info = FileProcessor.read_from_json_file("res://data/zh/npc.json")
 	setup_player(player_info)
 	
 func setup_player(json_info):
 	printed_name = json_info["name"]
-	nickname = json_info["nickname"]
+	# TODO: make it optional to read nickname, based on whether it exists or not
+	#nickname = json_info["nickname"]
 	states = json_info["states"]
 	body_parts = json_info["bodyParts"]
 	clothes = json_info["clothes"]
 	
 	# TODO: use signal instead
-	var ui = $"../MainUIMargin/UI/StatusDisplayArea/PlayerStatusContainer/StatusBarContainer/StatusBarLabel"
+	var ui = $"../MainUIMargin/UI/StatusDisplayArea/NpcStatusContainer/StatusBarContainer/StatusBarLabel"
 	ui.set_status_bar_label(states)
 	
 	
--- scripts/status_bar_label.gd
+++ scripts/status_bar_label.gd
@@ -2,18 +2,17 @@ extends RichTextLabel
 
 @export var status_bar_length = 12.0
 
-# TODO: Not done, needs to fix status format first
+func set_status_bar_label(state_dict: Dictionary):
+	var state_text = []
+	for state_key in state_dict.keys():
+		# TODO: read "hidden" key only when exists, otherwise print state by default
+		if state_dict[state_key]["hidden"] != true:
+			state_text.append( single_status_bar_text(state_dict, state_key) )
+	self.text = "\n".join(PackedStringArray(state_text))
 
-func set_status_bar_label(status_list: Dictionary):
-	var status_text = []
-	for status in status_list:
-		status_text.append( single_status_bar_text(status) )
-	self.text = "\n".join(PackedStringArray(status_text))
-	
-# TODO: Add type for status argument
-func single_status_bar_text(status) -> String:
-	var status_bar_count = floor(status["value"] * status_bar_length / 100.0)
+func single_status_bar_text(state_dict:Dictionary,key:String) -> String:
+	var status_bar_count = floor(state_dict[key]["value"] * status_bar_length / 100.0)
 	var status_bar_text = ""
 	status_bar_text += "█".repeat(status_bar_count) + "░".repeat(status_bar_length - status_bar_count)
-	return status.key + "： [color=%s]" % status["color"] + status_bar_text + "[/color] " + str(status["value"])
+	return key + "： [color=%s]" % state_dict[key]["color"] + status_bar_text + "[/color] " + "%2d" % int(state_dict[key]["value"])
 		
